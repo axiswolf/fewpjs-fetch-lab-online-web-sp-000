f983de99610456df34cafc961d85b8d1a77d68bf
Karen V <karenvo@usc.edu>
2019-12-28 08:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => console.log(json), renderBooks(json));
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json)
+      renderBooks(json)
+    });
 }
 
 function renderBooks(json) {
